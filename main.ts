d6fa12ea9ebc2f930ac8c7466c06a71ba31c192e
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-04-17 07:46:06 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env  node
+
+import inquirer from "inquirer"
+
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt ([
+
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to count the word:"
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+// Print the array count to the console
+
+console.log(words);
+
+// Print the word count of the sentence to the console
+
+console.log(`Your sentence word count is ${words.length}`);
+
+
+
+
